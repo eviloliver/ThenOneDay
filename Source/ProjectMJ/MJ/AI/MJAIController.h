a169d9abf94f580f7a13f5466f8e3b834e4a1f02
kimmjiinn <nankimminjin@gmail.com>
2025-06-13 12:12:09 +0900

    [Feat] AIController
    컨트롤러 C++ 생성

---
+++ Source/ProjectMJ/MJ/AI/MJAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "MJAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMJ_API AMJAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
