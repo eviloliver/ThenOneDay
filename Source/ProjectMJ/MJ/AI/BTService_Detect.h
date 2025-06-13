eaef5afb52889584868029638093524fd6610d2d
kimmjiinn <nankimminjin@gmail.com>
2025-06-14 00:11:09 +0900

    [Feat] Detect-Annotation
    주석 까먹어서 추가

--- Source/ProjectMJ/MJ/AI/BTService_Detect.h
+++ Source/ProjectMJ/MJ/AI/BTService_Detect.h
@@ -7,7 +7,11 @@
 #include "BTService_Detect.generated.h"
 
 /**
- * 
+ * Class Description: Service-플레이어를 감지하는 서비스 노드
+ * Author: Kim Minjin
+ * Created Date: 2025.06.13.
+ * Last Modified By: (Last Modifier)
+ * Last Modified Date: (Last Modified Date)
  */
 UCLASS()
 class PROJECTMJ_API UBTService_Detect : public UBTService_BlackboardBase
