e0f9f37dd7c239ea1c9eef74a934fee621a8b13d
kimmjiinn <nankimminjin@gmail.com>
2025-06-13 22:45:33 +0900

    [Feat] AIController
    기본적인 것 구현
    -BB, BT 초기화, AI 작동 및 중단, OnPossess

--- Source/ProjectMJ/MJ/AI/MJAIController.cpp
+++ Source/ProjectMJ/MJ/AI/MJAIController.cpp
@@ -2,4 +2,58 @@
 
 
 #include "MJ/AI/MJAIController.h"
+#include "BehaviorTree/BehaviorTree.h"
+#include "BehaviorTree/BlackboardData.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
+AMJAIController::AMJAIController()
+{
+	/*
+	 * TODO
+	 * AIPerception 추가
+	 */
+	static ConstructorHelpers::FObjectFinder<UBlackboardData> BBAssetRef(TEXT("/Script/AIModule.BlackboardData'/Game/AI/BB_MJMonster.BB_MJMonster'"));
+	if (nullptr!=BBAssetRef.Object)
+	{
+		BBAsset = BBAssetRef.Object;
+	}
+
+	static ConstructorHelpers::FObjectFinder<UBehaviorTree> BTAssetRef(TEXT("/Script/AIModule.BehaviorTree'/Game/AI/BT_MJMonster.BT_MJMonster'"));
+	if (nullptr!= BTAssetRef.Object)
+	{
+		BTAsset = BTAssetRef.Object;
+	}
+}
+
+void AMJAIController::RunAI()
+{
+	UBlackboardComponent* BlackboardPtr = Blackboard.Get();
+	if (UseBlackboard(BBAsset, BlackboardPtr))
+	{
+		// 시작 위치 값 설정
+		Blackboard->SetValueAsVector("HomePos", GetPawn()->GetActorLocation());
+		
+		bool RunResult = RunBehaviorTree(BTAsset);
+		ensure(RunResult);
+	}
+}
+
+void AMJAIController::StopAI()
+{
+	/*
+	 * TODO
+	 * Character가 죽을 때 호출해야 됨
+	 */
+	UBehaviorTreeComponent* BTComponent = Cast<UBehaviorTreeComponent>(BrainComponent);
+	if (BTComponent)
+	{
+		BTComponent->StopTree();
+	}
+}
+
+void AMJAIController::OnPossess(APawn* InPawn)
+{
+	Super::OnPossess(InPawn);
+
+	RunAI();
+}
--- Source/ProjectMJ/MJ/AI/MJAIController.h
+++ Source/ProjectMJ/MJ/AI/MJAIController.h
@@ -7,11 +7,30 @@
 #include "MJAIController.generated.h"
 
 /**
- * 
+ * Class Description: AIController-기본 작동
+ * Author: Kim Minjin
+ * Created Date: 2025.06.13.
+ * Last Modified By: (Last Modifier)
+ * Last Modified Date: (Last Modified Date)
  */
 UCLASS()
 class PROJECTMJ_API AMJAIController : public AAIController
 {
 	GENERATED_BODY()
-	
+
+public:
+	AMJAIController();
+
+	void RunAI();
+	void StopAI();
+
+protected:
+	virtual void OnPossess(APawn* InPawn) override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<class UBlackboardData> BBAsset;
+
+	UPROPERTY()
+	TObjectPtr<class UBehaviorTree> BTAsset;
 };
