aa6291d9da134eedf4c72143777861a39e077689
kimmjiinn <nankimminjin@gmail.com>
2025-06-19 23:52:42 +0900

    [Docs] Class Description
    추가하는 거 까먹음

--- Source/ProjectMJ/MJ/AI/BTTask_MJFindPatrolPos.h
+++ Source/ProjectMJ/MJ/AI/BTTask_MJFindPatrolPos.h
@@ -7,7 +7,11 @@
 #include "BTTask_MJFindPatrolPos.generated.h"
 
 /**
- * 
+ * Class Description: BBKey인 PatrolPos 설정 
+ * Author: Kim Minjin
+ * Created Date: 2025.06.19.
+ * Last Modified By: (Last Modifier)
+ * Last Modified Date: (Last Modified Date)
  */
 UCLASS()
 class PROJECTMJ_API UBTTask_MJFindPatrolPos : public UBTTaskNode
