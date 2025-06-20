6a35d113f7c75e7cbf7721138102a86b7cf64cdb
kimmjiinn <nankimminjin@gmail.com>
2025-06-20 16:45:42 +0900

    [Feat] TeamId Info
    None 추가

--- Source/ProjectMJ/MJ/AI/AIPerceptionInfo.h
+++ Source/ProjectMJ/MJ/AI/AIPerceptionInfo.h
@@ -6,7 +6,9 @@
 UENUM(BlueprintType)
 enum class ETeam_ID: uint8
 {
-	Player = 0,
-	NPC,
-	Monster
+	PLAYER = 0 UMETA(DisplayName = "PLAYER")/*플레이어*/,
+	NPC UMETA(DisplayName = "NPC")/*NPC*/,
+	MONSTER UMETA(DisplayName = "MONSTER")/*몬스터*/,
+	NONE = 255	UMETA(DisplayName = "NONE") /*기본값. Neutral로 처리됩니다*/
+
 };
