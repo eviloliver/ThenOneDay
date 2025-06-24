e8900b0c0e2c1984233f5c5325362d41298ab0e9
kimmjiinn <nankimminjin@gmail.com>
2025-06-24 17:34:20 +0900

    [Update] AIController Test
    자손 DetectionByAffiliation 반영되는 것 확인

--- Source/ProjectMJ/MJ/AI/MJMeleeMonsterAIController.cpp
+++ Source/ProjectMJ/MJ/AI/MJMeleeMonsterAIController.cpp
@@ -10,33 +10,15 @@
 
 AMJMeleeMonsterAIController::AMJMeleeMonsterAIController()
 {
-	/*
-	 * TODO
-	 * AIPerception 하드코딩 변경하기
-	 */
-	// AIPerception
-	// Sense-Sight
-	// AISenseConfig_Sight = CreateDefaultSubobject<UAISenseConfig_Sight>("AISenseConfig_Sight");
-	// AISenseConfig_Sight->SightRadius = 1000.0f;
-	// AISenseConfig_Sight->LoseSightRadius = 1500.0f;
-	// AISenseConfig_Sight->PeripheralVisionAngleDegrees = 60.0f;
-
-	/*
-	 * How To: 적, 중립, 아군 감지 여부
-	 */
-	/*
-	 * TODO
-	 * 설정 안 바뀌고 있음
-	 */
-	// DetectionByAffiliation.bDetectEnemies = true;
-	// DetectionByAffiliation.bDetectNeutrals = true;
-	// DetectionByAffiliation.bDetectFriendlies = false;
-	// AIPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
-
-
 }
 
 void AMJMeleeMonsterAIController::BeginPlay()
 {
 	Super::BeginPlay();
+
+	// 이러면 설정이 바뀌긴 함
+	// DetectionByAffiliation.bDetectEnemies = true;
+	// DetectionByAffiliation.bDetectNeutrals = true;
+	// DetectionByAffiliation.bDetectFriendlies = false;
+	// AIPerceptionComponent->ConfigureSense(*AISenseConfig_Sight);
 }
