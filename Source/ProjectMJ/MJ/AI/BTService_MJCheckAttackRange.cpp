40878c31702616a9cc98e4c3227123d57691b6b7
kimmjiinn <nankimminjin@gmail.com>
2025-06-25 23:53:55 +0900

    [Feat] Create Service Node: Check AttackRange
    타겟이 공격 범위에 들어와있는지 체크
    원거리 범위에 들어오면 원거리 키 true
    근거리 범위에 들어오면 근거리 키 true
    둘다 아니면 키 값 clear

---
+++ Source/ProjectMJ/MJ/AI/BTService_MJCheckAttackRange.cpp
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MJ/AI/BTService_MJCheckAttackRange.h"
+
+#include "AIController.h"
+#include "ProjectMJ.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "MJ/Interface/MJCharacterAIInterface.h"
+
+UBTService_MJCheckAttackRange::UBTService_MJCheckAttackRange()
+{
+	NodeName = "CheckAttackRange";
+	Interval = 0.1f;
+}
+
+void UBTService_MJCheckAttackRange::TickNode(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	/*
+	 * How to: 타겟이 몬스터의 공격 범위에 들어갔을 때 Maximum 혹은 Minimum 범위에 들어오면 블랙보드 키를 설정
+	 */
+	
+	Super::TickNode(OwnerComp, NodeMemory, DeltaSeconds);
+
+	APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (ControlledPawn == nullptr)
+	{
+		return;
+	}
+
+	IMJCharacterAIInterface* AIPawn = Cast<IMJCharacterAIInterface>(ControlledPawn);
+	if (AIPawn == nullptr)
+	{
+		return;
+	}
+
+	APawn* Target = Cast<APawn>(OwnerComp.GetBlackboardComponent()->GetValueAsObject("Target"));
+	if (Target == nullptr)
+	{
+		return;
+	}
+
+	OwnerComp.GetBlackboardComponent()->ClearValue("MinimumAttackRange");
+
+	float DistanceTo = FVector::Distance(Target->GetActorLocation(), ControlledPawn->GetActorLocation());
+	float DistanceToTarget = ControlledPawn->GetDistanceTo(Target);
+
+	// Minjin: 원거리 범위에 속할 경우
+	bool IsRange = DistanceToTarget <= AIPawn->GetAIMaximumAttackRange()
+		&& DistanceToTarget > AIPawn->GetAIMinimumAttackRange();
+	if (IsRange)
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool("MaximumAttackRange", true);
+		OwnerComp.GetBlackboardComponent()->ClearValue("MinimumAttackRange");
+		return;
+	}
+
+	// Minjin: 근거리 범위에 속할 경우
+	bool IsMelee = DistanceToTarget <= AIPawn->GetAIMinimumAttackRange();
+	if (IsMelee)
+	{
+		OwnerComp.GetBlackboardComponent()->SetValueAsBool("MinimumAttackRange", true);
+		OwnerComp.GetBlackboardComponent()->ClearValue("MaximumAttackRange");
+		return;
+	}
+
+	// Minjin: 아무 범위에도 안 속할 경우: 키 값 클리어
+	OwnerComp.GetBlackboardComponent()->ClearValue("MinimumAttackRange");
+	OwnerComp.GetBlackboardComponent()->ClearValue("MaximumAttackRange");
+}
