4b04a14ee18e0bf1a19509abf9f0f0c3c037c0ee
eviloliver <59022397+eviloliver@users.noreply.github.com>
2025-06-26 15:35:31 +0900

    Add: Setting For Data-Driven Develop
    - 데이터 기반 개발을 위한 세팅

---
+++ Source/ProjectMJ/DataTable/MJSkillDataRow.h
@@ -0,0 +1,69 @@
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "MJSkillDataRow.generated.h"
+
+UENUM(BlueprintType)
+enum class ERequiredWeapon : uint8
+{
+	None  UMETA(Hidden),
+	MeleeWeapon UMETA(DisplayName = "MeleeWeapon", ToolTip = "Sword, Spear, Dagger, ..."),
+	RangeWeapon UMETA(DisplayName = "RangeWeapon", ToolTip = "Bow, Cross bow, Gun, ..."),
+	MagicWeapon UMETA(DisplayName = "MagicWeapon", ToolTip = "Staff, Orb, Wand, ..."),
+	Anything UMETA(DisplayName = "Anything", ToolTip = "Anything"),
+};
+
+UENUM()
+enum class ESkillType : uint8
+{
+	None UMETA(Hidden),
+	Instant UMETA(DisplayName = "Instant", ToolTip = "Instantly Action Skill"),
+	Charge UMETA(DisplayName = "Charge", ToolTip = "Charging Action Skill"),
+	Passive UMETA(DisplayName = "Passibe", ToolTip = "Passive Skill"),
+};
+
+UENUM(BlueprintType)
+enum class ESkillTargetType : uint8
+{
+	None    UMETA(Hidden),
+	Single  UMETA(DisplayName = "Single", ToolTip = "Single Target"),
+	Area    UMETA(DisplayName = "AoE", ToolTip = "Area of Effect"),
+};
+
+class UAnimMontage;
+class UCurveTable;
+
+USTRUCT(BlueprintType)
+struct FMJSkillDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FName SkillID;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText SkillName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText SkillDescription;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ERequiredWeapon RequiredWeapon;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ESkillType SkillType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	ESkillTargetType SkillTargetType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UAnimMontage> ActionAnimMontage;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UCurveTable> SkillLevelDataTable;
+
+};
+	
