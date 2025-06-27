83b342922ffbecb5260f7856d837e26e46230810
Cha Tae Gwan <chataeg@gmail.com>
2025-06-27 12:20:28 +0900

    Docs: add missing annotation

--- Source/ProjectMJ/TG/Interface/MJInstancedActorInterface.h
+++ Source/ProjectMJ/TG/Interface/MJInstancedActorInterface.h
@@ -13,8 +13,12 @@ class UMJInstancedActorInterface : public UInterface
 	GENERATED_BODY()
 };
 
-/**
- * 
+/*
+ * Class Description: Interface which indicates whether inherited actor is restored or not
+ * Author: Cha Tae Gwan
+ * Created Date: 2025-06-27
+ * Last Modified By: Cha Tae Gwan
+ * Last Modified Date: 2025-06-27
  */
 class PROJECTMJ_API IMJInstancedActorInterface
 {
