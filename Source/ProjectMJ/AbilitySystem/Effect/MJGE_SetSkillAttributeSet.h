94947b2eb912bd0b847ac5397efc75a0801e2259
eviloliver <evil_oliver@kakao.com>
2025-06-30 06:48:31 +0900

    WIP: SetAttribute GE
    - 어트리뷰트 바꿔줄 GE 틀 만들기
    - 이거 SkillComponent에서 사용
    - 태그들 마저 만들기
    - 투사체 스킬
    - GE 적용

---
+++ Source/ProjectMJ/AbilitySystem/Effect/MJGE_SetSkillAttributeSet.h
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayEffect.h"
+#include "MJGE_SetSkillAttributeSet.generated.h"
+
+/**
+ * Class Description: Attribute Setter Using GameplayEffect
+ * Author: ½Åµ¿¹Î
+ * Created Date: 2025_06_29
+ * Last Modified By:
+ * Last Modified Date:
+ */
+UCLASS()
+class PROJECTMJ_API UMJGE_SetSkillAttributeSet : public UGameplayEffect
+{
+	GENERATED_BODY()
+
+public:
+	UMJGE_SetSkillAttributeSet();
+
+};
