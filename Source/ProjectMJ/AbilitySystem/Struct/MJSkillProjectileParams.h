af6d1c8b9c31e4c185d6d2fec005d22531bffd0a
eviloliver <evil_oliver@kakao.com>
2025-07-03 00:50:45 +0900

    Add: Param Struct Deader
    - 투사체에 넣어줄 Param 데이터 구조체 정의
    - 따로 헤더파일로 만듦

---
+++ Source/ProjectMJ/AbilitySystem/Struct/MJSkillProjectileParams.h
@@ -0,0 +1,69 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "MJSkillProjectileParams.generated.h"
+
+/**
+* Class Description : Projectile Param
+* Author : 신동민
+* Created Date : 2025.07.03
+* Last Modified By :
+* Last Modified Date :
+*/
+
+class UAbilitySystemComponent;
+class UGameplayEffect;
+
+USTRUCT(BlueprintType)
+struct FMJSkillProjectileParams
+{
+	GENERATED_BODY()
+
+	FMJSkillProjectileParams() {};
+
+	// Damage/Scaling
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float BaseDamage = 0.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float AttackDamageScaling = 1.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float AbilityPowerScaling = 1.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float LifeSteal = 0.f;
+
+    // Range
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float SkillRadius = 0.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float SkillRange = 0.f;
+
+    // Status Effect
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float StatusEffectChance = 0.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float StatusEffectDuration = 0.f;
+
+    // Projectile
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float ProjectileSpeed = 0.f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float ProjectileCount = 1.0f;
+
+    // ASC
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<UAbilitySystemComponent> SourceASC = nullptr;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TObjectPtr<UAbilitySystemComponent> TargetASC = nullptr;
+
+	// Effects
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TArray<TSubclassOf<UGameplayEffect>> DamageGameplayEffectClass = nullptr;
+
+};
