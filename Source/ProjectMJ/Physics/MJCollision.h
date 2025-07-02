f2f9d14a2e5fc98da75c228abbcdbd3e9d52644d
eviloliver <59022397+eviloliver@users.noreply.github.com>
2025-07-02 18:34:42 +0900

    Update: Collision Profile and Preset
    - 투사체 관련 콜리전 프로파일과 프리셋 추가

--- Source/ProjectMJ/Physics/MJCollision.h
+++ Source/ProjectMJ/Physics/MJCollision.h
@@ -5,4 +5,9 @@
 
 #define CPROFILE_MJCAPSULE TEXT("MJCapsule")
 #define CPROFILE_MJTRIGGER TEXT("MJTrigger")
+#define CRPOFILE_MJPROJECTILE TEXT("MJProjectile")
+
 #define CCHANNEL_MJAbilityTargetTrace ECC_GameTraceChannel1
+#define CCHANNEL_MJPROJECTILE ECC_GameTraceChannel2
+// ECollisionChannel::ECC_GameTraceChannel2
+// 이렇게 하는 방식 있는데 뭐가 다른지 찾아볼것
