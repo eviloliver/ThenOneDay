da2b141e4c93facfeac8cb6271d6cce9ed71aa07
Tae Gwan Cha <chataeg@gmail.com>
2025-07-07 13:28:36 +0900

    Feat: add MJ_SLOG onScreenDebugMessage Macro

--- Source/ProjectMJ/ProjectMJ.h
+++ Source/ProjectMJ/ProjectMJ.h
@@ -6,6 +6,8 @@
 
 #define LOG_CALLINFO ANSI_TO_TCHAR(__FUNCTION__)
 #define MJ_LOG(LogCat, Verbosity, Format, ...) UE_LOG(LogCat, Verbosity, TEXT("%s %s"), LOG_CALLINFO, *FString::Printf(Format, ##__VA_ARGS__))
+#define MJ_SLOG(Delay, Color, Format, ...)     GEngine->AddOnScreenDebugMessage(-1, Delay, Color, FString::Printf(Format));
+
 
 // cpp 파일에 DEFINE_LOG_CATEGORY 선언 필요함
 DECLARE_LOG_CATEGORY_EXTERN(LogMJ, Log, All);
