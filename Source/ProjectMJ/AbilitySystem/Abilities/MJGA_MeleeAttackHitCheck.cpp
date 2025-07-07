04ad7cd4120713fa0f29e4833498874adee76071
eviloliver <59022397+eviloliver@users.noreply.github.com>
2025-07-07 18:56:00 +0900

    WIP: Melee Attack Hit Check Apply Effect
    - 물리 공격에 데미지 이펙트 적용하기

--- Source/ProjectMJ/AbilitySystem/Abilities/MJGA_MeleeAttackHitCheck.cpp
+++ Source/ProjectMJ/AbilitySystem/Abilities/MJGA_MeleeAttackHitCheck.cpp
@@ -34,19 +34,27 @@ void UMJGA_MeleeAttackHitCheck::OnTraceResultCallback(const FGameplayAbilityTarg
 		FHitResult HitResult = UAbilitySystemBlueprintLibrary::GetHitResultFromTargetData(TargetDataHandle, 0);
 		// 이거 가까운거 하나만 검사하는건데 나중에 수정할 필요 있어보임
 
-		// UAbilitySystemComponent* SourceASC = GetAbilitySystemComponentFromActorInfo_Checked();
-		// UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitResult.GetActor());
+		UAbilitySystemComponent* SourceASC = GetAbilitySystemComponentFromActorInfo_Checked();
+		UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitResult.GetActor());
 		// null check 해야함
 		// GameplayCue 에 사용함
 
-		// const UMJCharacterAttributeSet* SourceCharacterAttributeSet = SourceASC->GetSet<UMJCharacterAttributeSet>();
-		// const UMJCharacterSkillAttributeSet* SourceCharacterSkillAttributeSet = SourceASC->GetSet<UMJCharacterSkillAttributeSet>();
+		const UMJCharacterAttributeSet* SourceCharacterAttributeSet = SourceASC->GetSet<UMJCharacterAttributeSet>();
+		const UMJCharacterSkillAttributeSet* SourceCharacterSkillAttributeSet = SourceASC->GetSet<UMJCharacterSkillAttributeSet>();
 		// null check 해야함
 		// GE 없이 테스트 할 때 사용해 볼 것
 
 		FGameplayEffectSpecHandle EffectSpecHandle = MakeOutgoingGameplayEffectSpec(AttackDamageEffect);
 		if (EffectSpecHandle.IsValid())
 		{
+			MJ_LOG(LogMJ, Warning, TEXT("GA: EffectSpecHandle.Data (before set) address: %p"), EffectSpecHandle.Data.Get());
+
+			EffectSpecHandle.Data->SetSetByCallerMagnitude(FGameplayTag::RequestGameplayTag(FName("Data.Skill.BaseDamage")), SourceCharacterSkillAttributeSet->GetBaseDamage());
+			EffectSpecHandle.Data->SetSetByCallerMagnitude(FGameplayTag::RequestGameplayTag(FName("Data.Skill.AttackDamageScaling")), SourceCharacterSkillAttributeSet->GetAttackDamageScaling());
+			EffectSpecHandle.Data->SetSetByCallerMagnitude(FGameplayTag::RequestGameplayTag(FName("Data.Skill.AbilityPowerScaling")), SourceCharacterSkillAttributeSet->GetAbilityPowerScaling());
+			MJ_LOG(LogMJ, Warning, TEXT("GA: BaseDamage after setting on SpecHandle.Data: %f"), EffectSpecHandle.Data->GetSetByCallerMagnitude(FGameplayTag::RequestGameplayTag(FName("Data.Skill.BaseDamage")), false));
+			MJ_LOG(LogMJ, Warning, TEXT("GA: EffectSpecHandle.Data (after set) address: %p"), EffectSpecHandle.Data.Get());
+			MJ_LOG(LogMJ,Warning, TEXT("%f %f %f"), SourceCharacterSkillAttributeSet->GetBaseDamage(), SourceCharacterSkillAttributeSet->GetAttackDamageScaling(), SourceCharacterSkillAttributeSet->GetAbilityPowerScaling())
 			ApplyGameplayEffectSpecToTarget(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, EffectSpecHandle, TargetDataHandle);
 
 			// TODO : GameplayCue
@@ -61,6 +69,7 @@ void UMJGA_MeleeAttackHitCheck::OnTraceResultCallback(const FGameplayAbilityTarg
 
 		if (EffectSpecHandle.IsValid())
 		{
+
 			ApplyGameplayEffectSpecToTarget(CurrentSpecHandle, CurrentActorInfo, CurrentActivationInfo, EffectSpecHandle, TargetDataHandle);
 
 			// TODO : GameplayCue
