a79b1e69dabe02800bb38662078c005dcd78e6ff
kimmjiinn <nankimminjin@gmail.com>
2025-07-09 10:41:55 +0900

    [Docs] Annotation
    노드에 주석추가

--- Source/ProjectMJ/MJ/AI/BTTask_MJPlayAppearance.h
+++ Source/ProjectMJ/MJ/AI/BTTask_MJPlayAppearance.h
@@ -7,7 +7,11 @@
 #include "BTTask_MJPlayAppearance.generated.h"
 
 /**
- * 
+ * Class Description: 맵에 몬스터 모습 등장
+ * Author: Kim Minjin
+ * Created Date: 2025.07.08.
+ * Last Modified By: (Last Modifier)
+ * Last Modified Date: (Last Modified Date)
  */
 UCLASS()
 class PROJECTMJ_API UBTTask_MJPlayAppearance : public UBTTask_PlayAnimation
