5bdfe438688b31b6a6081ac72840eca72a24187c
diduo7 <ehfpal5711@naver.com>
2025-07-11 12:31:33 +0900

    [Add] Dialogue Font
    - 폰트 디테일 변경 (글꼴, 사이즈)
    - 다이어로그 이미지 투명도 디테일 변경

--- Source/ProjectMJ/Dialogue/MJBacklogWidget.cpp
+++ Source/ProjectMJ/Dialogue/MJBacklogWidget.cpp
@@ -4,7 +4,9 @@
 #include "Dialogue/MJBacklogWidget.h"
 #include "Components/TextBlock.h"
 #include "Components/ScrollBox.h"
+#include "Components/ScrollBoxSlot.h"
 #include "Dialogue/MJDialogueRow.h"
+#include "Fonts/SlateFontInfo.h"
 
 void UMJBacklogWidget::AddLine(const FMJDialogueRow& Row) // 함수명 수정하기 > AddLine으로
 {
@@ -15,12 +17,25 @@ void UMJBacklogWidget::AddLine(const FMJDialogueRow& Row) // 함수명 수정하
 	
 	if (NewTextBlock)
 	{
+		// 폰트 변경
+		FSlateFontInfo FontInfo;
+		FontInfo.FontObject = LoadObject<UObject>(nullptr, TEXT("/Game/UI/Font/CookieRun_Regular_Font.CookieRun_Regular_Font"));
+		FontInfo.Size = 15;
+		NewTextBlock->SetFont(FontInfo);
+		
+		
 		NewTextBlock->SetText(FText::FromString(Line));
 		BacklogScrollBox->AddChild(NewTextBlock);
 
-		// 텍스트 색 변경방법
-		FSlateColor SetColor = FSlateColor(FLinearColor::Blue);
+		// 텍스트 색 변경
+		FSlateColor SetColor = FSlateColor(FLinearColor::White);
 		NewTextBlock->SetColorAndOpacity(SetColor);
+
+		// 대사 행 간격
+		if (UScrollBoxSlot* ScrollSlot = Cast<UScrollBoxSlot>(NewTextBlock->Slot))
+		{
+			ScrollSlot->SetPadding(FMargin(20, 5, 20, 5));
+		}
 	}
 }
 
