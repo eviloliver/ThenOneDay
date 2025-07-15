2f47dac45b0082d1739ddf687d6ede50b0236176
eviloliver <evil_oliver@kakao.com>
2025-07-16 04:29:14 +0900

    Add: Enemy DataTable
    - 적 능력치 관련 데이터 테이블 추가
    - TODO:
      - 몬스터 생성자와 BeginPlay에 레벨을 전해주면 DataTable 적용(생성자 안 되었던거 같음)
      - 다른 브랜치에 있는 태그와 합쳐야 함 + 기본 공격 구현 및 장착
      - SkillDataRow도 수정하자
      - Player Data Row도 구축
      - CurveTable 함수로 자동으로 하는거 있는지 알아볼것

---
+++ Source/ProjectMJ/DataTable/MJEnemyDataRow.h
@@ -0,0 +1,70 @@
+﻿
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "GameplayTagContainer.h"
+#include "MJEnemyDataRow.generated.h"
+
+/**
+ * Class Description: Enemy DataTable Base
+ * Author: 신동민
+ * Created Date: 2025.07.16
+ * Modified By:
+ * Modified Date:
+ */
+
+class UCurveTable;
+class USkeletalMesh;
+class UGameplayEffect;
+
+USTRUCT(BlueprintType)
+struct FMJEnemyDataRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Categories = "Enemy.Name"))
+	FGameplayTag DefaultEnemyTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText DisplayName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FText Description;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Categories = "Enemy.Type"))
+	FGameplayTag TypeTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Categories = "Enemy.Species"))
+	FGameplayTag SpeciesTag;
+
+	// 배우고 있는 스킬 관련
+	// Skill Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Categories = "Skill.Normal"))
+	FGameplayTag NormalAttackTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (Categories = "Skill"))
+	FGameplayTag IdentitySkillTag;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 SkillLevel = 1;
+
+	// 기본 스탯
+	// Basic Stat Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<UCurveTable> AttributeCurve;
+
+	// 외형
+	// Visual Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TSoftObjectPtr<USkeletalMesh> SkeletalMesh;
+
+	// TODO: 사운드나 아이콘 등등
+
+	// 초기 세팅값
+	// Initial Section
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TArray<TSubclassOf<UGameplayEffect>> InitialEffects;
+
+};
