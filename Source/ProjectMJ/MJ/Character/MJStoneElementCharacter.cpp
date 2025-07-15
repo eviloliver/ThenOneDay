a83a76eed5b4162d846fce62f1782cc10aa9567d
kimmjiinn <nankimminjin@gmail.com>
2025-07-16 00:46:22 +0900

    [Test] Monster Refactor
    태그를 가져와 BTTask에서 ActiavateSkill을 해보려고 하는 중

--- Source/ProjectMJ/MJ/Character/MJStoneElementCharacter.cpp
+++ Source/ProjectMJ/MJ/Character/MJStoneElementCharacter.cpp
@@ -6,13 +6,13 @@
 void AMJStoneElementCharacter::BeginPlay()
 {
 	Super::BeginPlay();
-
-	SkillComponent->LearnSkill(FGameplayTag::RequestGameplayTag(FName("Skill.Instant.StoneElementalMeleeAttack")));
-	SkillComponent->EquipSkill(FGameplayTag::RequestGameplayTag(FName("Skill.Instant.StoneElementalMeleeAttack")));
+	AttackTag = FGameplayTag::RequestGameplayTag(FName("Skill.Instant.StoneElementalMeleeAttack"));
+	SkillComponent->LearnSkill(AttackTag);
+	SkillComponent->EquipSkill(AttackTag);
 	
 }
 
 void AMJStoneElementCharacter::AttackByAI()
 {
-	SkillComponent->ActivateSkill(FGameplayTag::RequestGameplayTag(FName("Skill.Instant.StoneElementalMeleeAttack")));
+	SkillComponent->ActivateSkill(AttackTag);
 }
