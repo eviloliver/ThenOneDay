8da6b8dd69bcc9eae5b991251a82df7da2e563d3
kimmjiinn <nankimminjin@gmail.com>
2025-07-16 23:36:42 +0900

    [Feat] BTTasks: Attack
    EquippedSkillMap에 대한 Getter추가, EquippedSkillMap을 통해 태그 가져옴
    -각 속성에 대한 공격 태스크 생성

---
+++ Source/ProjectMJ/MJ/AI/BTTask_MJInstantAttack.cpp
@@ -0,0 +1,59 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MJ/AI/BTTask_MJInstantAttack.h"
+#include "AIController.h"
+#include "MJ/Character/MJMonsterCharacter.h"
+#include "BehaviorTree/BlackboardComponent.h"
+#include "AbilitySystemComponent.h"
+#include "Abilities/GameplayAbility.h"
+#include "Character/Component/MJSkillComponentBase.h"
+#include "ProjectMJ.h"
+
+UBTTask_MJInstantAttack::UBTTask_MJInstantAttack()
+{
+	NodeName = TEXT("InstantAttack");
+}
+
+EBTNodeResult::Type UBTTask_MJInstantAttack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	APawn* ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	if (ControlledPawn == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	AMJMonsterCharacter* Monster = Cast<AMJMonsterCharacter>(ControlledPawn);
+	if (Monster == nullptr)
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	UAbilitySystemComponent* ASC = Monster->GetAbilitySystemComponent();
+	UMJSkillComponentBase* SkillComponent = Monster->GetSkillComponent();
+	
+	FGameplayTag SkillTypeTag = FGameplayTag::RequestGameplayTag(FName("Skill.Instant"));
+	FGameplayTag AttackTag = SkillComponent->GetEquippedSkillMap()[SkillTypeTag];
+
+	if (!AttackTag.IsValid())
+	{
+		return EBTNodeResult::Failed;
+	}
+
+	FDelegateHandle Handle;
+	Handle = ASC->OnAbilityEnded.AddLambda(
+	[&, AttackTag, Handle](const FAbilityEndedData& EndedData)
+	{
+		MJ_LOG(LogMJ, Error,TEXT("A"));
+		if (EndedData.AbilityThatEnded->AbilityTags.HasTagExact(AttackTag))
+		{
+			MJ_LOG(LogMJ, Error,TEXT("AA"));
+			FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+			ASC->OnAbilityEnded.Remove(Handle);
+		}
+	});
+	MJ_LOG(LogMJ, Error,TEXT("AAA"));
+	SkillComponent->ActivateSkill(AttackTag);
+	
+	return EBTNodeResult::InProgress;
+}
