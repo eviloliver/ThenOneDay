7217a88c91ca33da6439f393c9a4a689e54d191c
Tae Gwan Cha <chataeg@gmail.com>
2025-07-21 04:02:22 +0900

    Refactor: refactor MainMenu UI

--- Source/ProjectMJ/TG/UI/MJMainMenuWidget.cpp
+++ Source/ProjectMJ/TG/UI/MJMainMenuWidget.cpp
@@ -4,7 +4,6 @@
 #include "TG/UI/MJMainMenuWidget.h"
 
 #include "MJSettingsWidget.h"
-#include "ProjectMJ.h"
 #include "Components/Button.h"
 #include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
@@ -27,11 +26,28 @@ void UMJMainMenuWidget::NativeConstruct()
 		InputMode.SetLockMouseToViewportBehavior(EMouseLockMode::LockInFullscreen);
    
 		PC->SetInputMode(InputMode);
+
+		NewGamePopUpWidget = Cast<UMJChildMenuBaseWidget>(CreateWidget(PC, NewGamePopUpWidgetClass));
+		if (NewGamePopUpWidget)
+		{
+			NewGamePopUpWidget->AddToViewport(2);
+			NewGamePopUpWidget->SetParentWidget(this);
+			NewGamePopUpWidget->SetVisibility(ESlateVisibility::Hidden);
+		}
+
+		LoadGameWidget =  Cast<UMJChildMenuBaseWidget>(CreateWidget(PC,LoadGameWidgetClass));
+		if (LoadGameWidget)
+		{
+			LoadGameWidget->AddToViewport(2);
+			LoadGameWidget->SetParentWidget(this);
+			LoadGameWidget->SetVisibility(ESlateVisibility::Hidden);
+		}
 		
-		SettingsWidget = CreateWidget(PC,SettingsWidgetClass);
+		SettingsWidget =  Cast<UMJChildMenuBaseWidget>(CreateWidget(PC,SettingsWidgetClass));
 		if (SettingsWidget)
 		{
 			SettingsWidget->AddToViewport(2);
+			SettingsWidget->SetParentWidget(this);
 			SettingsWidget->SetVisibility(ESlateVisibility::Hidden);
 		}
 	}
@@ -39,37 +55,37 @@ void UMJMainMenuWidget::NativeConstruct()
 
 void UMJMainMenuWidget::OnClicked_NewGame()
 {
-	RemoveFromParent();
-
-	if (APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0))
-	{
-		FInputModeGameAndUI InputModeGameAndUI;
-		PC->SetInputMode(InputModeGameAndUI);
-	}
+	
+	
+	SetVisibility(ESlateVisibility::Hidden);
+	NewGamePopUpWidget->SetVisibility(ESlateVisibility::Visible);
+	
 
 	// Move to TG_Town
 	// @fixme : maybe there`s other good method
-	UGameplayStatics::OpenLevel(this,TEXT("TG_Town"));
-	
+	//UGameplayStatics::OpenLevel(this,TEXT("TG_Town"));
 }
 
 void UMJMainMenuWidget::OnClicked_LoadGame()
 {
-	MJ_SLOG(3.0f,FColor::Blue,TEXT("Not Implemented Yet...^^"));
+	SetVisibility(ESlateVisibility::Hidden);
+
+	if (LoadGameWidget)
+	{
+		LoadGameWidget->SetVisibility(ESlateVisibility::Visible);
+	}
 }
 
 void UMJMainMenuWidget::OnClicked_Settings()
 {
 	SetVisibility(ESlateVisibility::Hidden);
 	
-	UMJSettingsWidget* MJSettingsWidget = Cast<UMJSettingsWidget>(SettingsWidget);
-	
-	if (MJSettingsWidget)
+	if (SettingsWidget)
 	{
-		MJSettingsWidget->SetParentWidget(this);
-		MJSettingsWidget->SetVisibility(ESlateVisibility::Visible);
+		SettingsWidget->SetVisibility(ESlateVisibility::Visible);
 	}
 }
+
 void UMJMainMenuWidget::OnClicked_Quit()
 {
 	UKismetSystemLibrary::QuitGame(this,nullptr,EQuitPreference::Quit,false);
