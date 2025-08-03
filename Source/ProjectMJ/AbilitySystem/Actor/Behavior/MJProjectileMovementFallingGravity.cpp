51f6df66602bb2e5365eb23faaccf5dc2e74216d
eviloliver <evil_oliver@kakao.com>
2025-08-04 05:42:00 +0900

    Feat: Falling Gravity Movement Behavior
    - 중력이 적용된 투사체

---
+++ Source/ProjectMJ/AbilitySystem/Actor/Behavior/MJProjectileMovementFallingGravity.cpp
@@ -0,0 +1,26 @@
+﻿// ThenOneDayStudio
+
+
+#include "AbilitySystem/Actor/Behavior/MJProjectileMovementFallingGravity.h"
+
+#include "ProjectMJ.h"
+#include "AbilitySystem/Actor/MJProjectileBase.h"
+
+UMJProjectileMovementFallingGravity::UMJProjectileMovementFallingGravity()
+{
+}
+
+void UMJProjectileMovementFallingGravity::Move(AMJProjectileBase* InProjectile, float DeltaSeconds)
+{
+	if (!OwnerProjectile)
+	{
+		MJ_LOG(LogMJ, Warning, TEXT("Not Exist OwnerProjectile"));
+		return;
+	}
+
+	FVector Gravity = FVector(0.0f, 0.0f, GetWorld()->GetGravityZ());
+	Velocity += Gravity * DeltaSeconds;
+
+	FVector NewLocation = OwnerProjectile->GetActorLocation() + Velocity * DeltaSeconds;
+	OwnerProjectile->SetActorLocation(NewLocation, true);
+}
---
+++ Source/ProjectMJ/AbilitySystem/Actor/Behavior/MJProjectileMovementFallingGravity.h
@@ -0,0 +1,28 @@
+﻿// ThenOneDayStudio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Actor/Behavior/MJProjectileMovementFalling.h"
+#include "MJProjectileMovementFallingGravity.generated.h"
+
+/**
+ * Class Description: 하늘에서 떨어지는 투사체 (중력 적용)
+ * Author: 신동민
+ * Created Date: 2025.08.04
+ * Description of Change:
+ * Modified By:
+ * Modified Date:
+ */
+UCLASS()
+class PROJECTMJ_API UMJProjectileMovementFallingGravity : public UMJProjectileMovementFalling
+{
+	GENERATED_BODY()
+
+public:
+	UMJProjectileMovementFallingGravity();
+
+	virtual void Move(AMJProjectileBase* InProjectile, float DeltaSeconds) override;
+protected:
+
+};
