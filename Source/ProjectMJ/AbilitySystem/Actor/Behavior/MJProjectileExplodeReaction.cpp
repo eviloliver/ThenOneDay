10f867fb013f96d24e3df1d2a6f2ff6a08228f64
eviloliver <59022397+eviloliver@users.noreply.github.com>
2025-08-04 21:26:14 +0900

    WIP: GameplayCue Explosion
    - 폭발 GameplayCue 구현
    - 작업중이던 BP 저장

--- Source/ProjectMJ/AbilitySystem/Actor/Behavior/MJProjectileExplodeReaction.cpp
+++ Source/ProjectMJ/AbilitySystem/Actor/Behavior/MJProjectileExplodeReaction.cpp
@@ -3,17 +3,70 @@
 
 #include "AbilitySystem/Actor/Behavior/MJProjectileExplodeReaction.h"
 
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "GameplayEffectTypes.h"
+#include "ProjectMJ.h"
 #include "AbilitySystem/Actor/MJProjectileBase.h"
 #include "Kismet/GameplayStatics.h"
 
-auto UMJProjectileExplodeReaction::OnProjectileReact(AMJProjectileBase* Projectile, AActor* HitActor,
-                                                     const FHitResult& Hit) -> void
+UMJProjectileExplodeReaction::UMJProjectileExplodeReaction()
 {
-	Super::OnProjectileReact(Projectile, HitActor, Hit);
-
 }
 
-void UMJProjectileExplodeReaction::InitReaction(AMJProjectileBase* Projectile)
+void UMJProjectileExplodeReaction::InitReaction(AMJProjectileBase* InProjectile)
 {
-	Super::InitReaction(Projectile);
+	OwnerProjectile = InProjectile;
+
+}
+void UMJProjectileExplodeReaction::OnProjectileReact(AMJProjectileBase* InProjectile, AActor* HitActor, const FHitResult& Hit)
+{
+	if (!OwnerProjectile)
+	{
+		MJ_LOG(LogMJ, Warning, TEXT("Not Exist OwnerProjectile"));
+		return;
+	}
+
+	if (!HitActor)
+	{
+		MJ_LOG(LogMJ, Warning, TEXT("Not Exist HitActor"));
+		return;
+	}
+
+	if (HitActor == OwnerProjectile->ProjectileParams.SourceASC->GetAvatarActor())
+	{
+		MJ_LOG(LogMJ, Warning, TEXT("HitActor is User"));
+		return;
+	}
+
+	// 이거 Cue 억지로 쓴 느낌
+	// 그냥 Niagara System 달아줘서 재생했어도 될거 같음
+	FGameplayCueParameters CueParams;
+	CueParams.Location = OwnerProjectile->GetActorLocation();
+	OwnerProjectile->ProjectileParams.SourceASC->ExecuteGameplayCue(GameplayCueTag, CueParams);
+
+	UAbilitySystemComponent* TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(HitActor);
+	if (!TargetASC)
+	{
+		MJ_LOG(LogMJ, Warning, TEXT("Not Exist TargetASC"));
+		return;
+	}
+
+	FGameplayEffectContextHandle EffectContext = OwnerProjectile->ProjectileParams.SourceASC->MakeEffectContext();
+	EffectContext.AddSourceObject(this);
+
+
+	Explode();
+}
+
+void UMJProjectileExplodeReaction::Explode()
+{
+	if (!OwnerProjectile)
+	{
+		MJ_LOG(LogMJ, Warning, TEXT("Not Exist OwnerProjectile"));
+		return;
+	}
+
+	FGameplayCueParameters CueParams;
+
 }
