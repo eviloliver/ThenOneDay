d34aa39bb826e794b80fbeccee0ade90fa4c5242
eviloliver <59022397+eviloliver@users.noreply.github.com>
2025-08-04 19:44:42 +0900

    WIP: LifeSpan And Pierce
    - 투사체 생명시간
    - 관통
    - 벽 충돌

--- Source/ProjectMJ/AbilitySystem/Actor/MJProjectileBase.cpp
+++ Source/ProjectMJ/AbilitySystem/Actor/MJProjectileBase.cpp
@@ -19,13 +19,18 @@ AMJProjectileBase::AMJProjectileBase()
 	// Sphere Section
 	Sphere = CreateDefaultSubobject<USphereComponent>("Projectile");
 	SetRootComponent(Sphere);
+
 	Sphere->SetCollisionObjectType(CCHANNEL_MJPROJECTILE);
-	Sphere->SetCollisionProfileName(CRPOFILE_MJPROJECTILE);
+
+	Sphere->SetCollisionEnabled(ECollisionEnabled::Type::QueryAndPhysics);
+	Sphere->SetCollisionResponseToAllChannels(ECR_Ignore);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Block);
+	Sphere->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Block);
+	Sphere->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
 
 	// Niagara Section
 	NiagaraComponent = CreateDefaultSubobject<UNiagaraComponent>("NiagaraComponent");
 	NiagaraComponent->SetupAttachment(RootComponent);
-
 }
 
 void AMJProjectileBase::Tick(float DeltaSeconds)
@@ -69,6 +74,7 @@ void AMJProjectileBase::InitProjectile(const FMJSkillProjectileParams& InParams,
 	if (ProjectileParams.SkillRadius > 0.0f)
 	{
 		Sphere->SetSphereRadius(ProjectileParams.SkillRadius);
+
 	}
 }
 
@@ -76,12 +82,32 @@ void AMJProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();
 	Sphere->OnComponentBeginOverlap.AddDynamic(this, &AMJProjectileBase::OnSphereOverlap);
+	Sphere->OnComponentHit.AddDynamic(this, &AMJProjectileBase::OnSphereHit);
 
+	if (ProjectileParams.LifeSpan > 0.0f)
+	{
+		SetLifeSpan(ProjectileParams.LifeSpan);
+	}
 }
 
 void AMJProjectileBase::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
+	bool bIsLastPierce = false;
+
+	if (ProjectileParams.PierceCount > 0)
+	{
+		--ProjectileParams.PierceCount;
+		if (ProjectileParams.PierceCount <= 0)
+		{
+			bIsLastPierce = true;
+		}
+	}
+	else
+	{
+		bIsLastPierce = true;
+	}
+
 	for (const TObjectPtr <UMJProjectileReactionBehaviorBase> ReactionBehavior : ReactionBehaviors)
 	{
 		if (ReactionBehavior)
@@ -89,4 +115,22 @@ void AMJProjectileBase::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent
 			ReactionBehavior->OnProjectileReact(this, OtherActor, SweepResult);
 		}
 	}
+
+	if (bIsLastPierce)
+	{
+		Destroy();
+	}
+}
+
+void AMJProjectileBase::OnSphereHit(UPrimitiveComponent* HitComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	for (const TObjectPtr <UMJProjectileReactionBehaviorBase> ReactionBehavior : ReactionBehaviors)
+	{
+		if (ReactionBehavior)
+		{
+			ReactionBehavior->OnProjectileReact(this, OtherActor, Hit);
+		}
+	}
+	Destroy();
 }
