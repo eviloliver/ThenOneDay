1359a31291c7b3acc5945d6a4e2b58e032f44aa8
eviloliver <59022397+eviloliver@users.noreply.github.com>
2025-08-09 18:42:23 +0900

    Feat: AI Charge Skill
    - AI 전용 차징 스킬 구현

--- Source/ProjectMJ/AbilitySystem/Abilities/MJGA_AIActionChargeAbility.h
+++ Source/ProjectMJ/AbilitySystem/Abilities/MJGA_AIActionChargeAbility.h
@@ -7,11 +7,35 @@
 #include "MJGA_AIActionChargeAbility.generated.h"
 
 /**
- * 
+ * Class Description: AI 전용 Charge 스킬
+ * Author: 신동민
+ * Created Date: 2025.08.09
+ * Description of Change:
+ * Modified By:
+ * Modified Date:
  */
 UCLASS()
 class PROJECTMJ_API UMJGA_AIActionChargeAbility : public UMJGA_ChargeSkill
 {
 	GENERATED_BODY()
-	
+
+public:
+	UMJGA_AIActionChargeAbility();
+
+	virtual void ActivateAbility(const FGameplayAbilitySpecHandle Handle, const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo, const FGameplayEventData* TriggerEventData) override;
+
+protected:
+
+	UFUNCTION()
+	void OnChargeDelayFinished();
+
+	UFUNCTION()
+	void OnReleaseMontageComplete();
+
+	void PlayReleaseMontage();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float ChargeTime;
+
 };
