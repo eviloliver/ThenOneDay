6463e3f6880bcd7a147b024a054806edf444a1c9
diduo7 <ehfpal5711@naver.com>
2025-08-11 03:45:16 +0900

    [WIP] SkillWidget

---
+++ Source/ProjectMJ/UI/Skill/MJSkillWidget.h
@@ -0,0 +1,18 @@
+// ThenOneDayStudio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MJSkillWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMJ_API UMJSkillWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+	
+};
