7fc7603994412d53a9eef216905fea39e18a7707
jinjucastle <guswlwl22@naver.com>
2025-08-11 14:18:19 +0900

    Feat: add Spider Minion Class

---
+++ Source/ProjectMJ/MJ/Character/MJSpiderMinionCharacter.h
@@ -0,0 +1,17 @@
+// ThenOneDayStudio
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MJ/Character/MJMonsterCharacter.h"
+#include "MJSpiderMinionCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTMJ_API AMJSpiderMinionCharacter : public AMJMonsterCharacter
+{
+	GENERATED_BODY()
+	
+};
