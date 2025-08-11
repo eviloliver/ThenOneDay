eaa18900d12d69dfe235a2124535f7407bb669b7
eviloliver <evil_oliver@kakao.com>
2025-08-12 05:10:08 +0900

    Feat: Create Trace Channel
    - 땅 관련한 트레이스 채널 생성

--- Source/ProjectMJ/Physics/MJCollision.h
+++ Source/ProjectMJ/Physics/MJCollision.h
@@ -9,5 +9,6 @@
 
 #define CCHANNEL_MJAbilityTargetTrace ECC_GameTraceChannel1
 #define CCHANNEL_MJPROJECTILE ECC_GameTraceChannel2
+#define CCHANNEL_MJGROUND ECC_GameTraceChannel3
 // ECollisionChannel::ECC_GameTraceChannel2
 // 이렇게 하는 방식 있는데 뭐가 다른지 찾아볼것
